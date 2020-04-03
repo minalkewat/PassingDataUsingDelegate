49ce703f43637d257c057fb2c154201f85652f75
minal <minalkewat@gmail.com>
2020-04-03 23:28:05 +0530

    Initial Commit

---
+++ PassingDataUsingDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataUsingDelegate
+//
+//  Created by apple on 03/04/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
